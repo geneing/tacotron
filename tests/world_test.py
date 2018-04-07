c9c45d41eb11dfac64433933c9cbc1656a92f313
geneing <geneing@gmail.com>
2018-04-06 22:48:11 -0700

    Added wavernn test

--- tests/world_test.py
+++ tests/world_test.py
@@ -7,15 +7,17 @@ import pyworld as pw
 from pylab import *
 import scipy.io.wavfile as wav
 
+import simpleaudio as sa
+import librosa
+
 
 #%%
-import librosa
 
 ref_level_db=20
 num_freq=1025
-sample_rate=20000
-frame_length_ms=50
-frame_shift_ms=12.5
+
+frame_length_ms=5
+frame_shift_ms=1.25
 min_level_db=-100
 num_mels=80
 num_freq=1025
@@ -24,14 +26,14 @@ max_mel_freq=7600
 
 power=1.5
 
-def _stft_parameters():
+def _stft_parameters(sample_rate):
   n_fft = (num_freq - 1) * 2
   hop_length = int(frame_shift_ms / 1000 * sample_rate)
   win_length = int(frame_length_ms / 1000 * sample_rate)
   return n_fft, hop_length, win_length
 
-def spectrogram(y):
-  D = _stft(y)
+def spectrogram(y, fs):
+  D = _stft(y, fs)
   S = _amp_to_db(np.abs(D)) - ref_level_db
   return _normalize(S)
 
@@ -41,18 +43,18 @@ def inv_spectrogram(spectrogram):
   S = _db_to_amp(_denormalize(spectrogram) + ref_level_db)  # Convert back to linear
   return _griffin_lim(S ** power)          # Reconstruct phase
 
-def melspectrogram(y):
-  D = _stft(y)
+def melspectrogram(y, fs):
+  D = _stft(y, fs)
   S = _amp_to_db(_linear_to_mel(np.abs(D)))
   return _normalize(S)
 
-def _stft(y):
-  n_fft, hop_length, win_length = _stft_parameters()
+def _stft(y, fs):
+  n_fft, hop_length, win_length = _stft_parameters(fs)
   return librosa.stft(y=y, n_fft=n_fft, hop_length=hop_length, win_length=win_length)
 
 
-def _istft(y):
-  _, hop_length, win_length = _stft_parameters()
+def _istft(y, fs):
+  _, hop_length, win_length = _stft_parameters(fs)
   return librosa.istft(y, hop_length=hop_length, win_length=win_length)
 
 _mel_basis = None
@@ -101,10 +103,67 @@ sp_coded=pw.code_spectral_envelope(sp,fs,128)
 y = pw.synthesize(f0, sp, ap, fs, pw.default_frame_period)
 #%%
 
-stft_x = _stft(x)
-stft_y = _stft(y)
+stft_x = _stft(x, fs)
+stft_y = _stft(y, fs)
 
 #%%
 
 mel_x = melspectrogram(_normalize(x))
 mel_y = melspectrogram(_normalize(y))
+
+#%%
+
+def encode(x, fs, frame_period):
+    x=x.astype(float)
+    f0, sp, ap = pw.wav2world(x, fs, frame_period)
+    
+    nframes = int(fs * f0.size * frame_period/1000)
+    x = np.pad(x,(0,nframes-x.size), 'constant')
+
+    f01, sp1, ap1 = pw.wav2world(x, fs, frame_period)
+    w_x=np.hstack([f01[:,np.newaxis],sp1,ap1])
+
+    y = pw.synthesize(f0*(1+0*np.random.normal(0,.05,size=f0.shape)), 
+                      sp*(1+1*np.random.normal(0,.2,size=sp.shape)), 
+                      ap*(1+1*np.random.normal(0,.2,size=ap.shape)), fs, frame_period)
+
+    f02, sp2, ap2 = pw.wav2world(y, fs, frame_period)
+    w_y = np.hstack([f02[:,np.newaxis],sp2,ap2])
+    
+    stft_x = np.abs(_stft(x, fs))
+    stft_y = np.abs(_stft(y, fs))
+    
+    return (x,y,f0,sp,ap,stft_x,stft_y,w_x,w_y)
+    
+
+path="/home/eugening/Neural/MachineLearning/Speech/TrainingData/LJSpeech-1.0/wavs/LJ001-0001.wav"
+(fs,x) = wav.read(path)
+
+(x,y,f0,sp,ap,stft_x,stft_y,w_x,w_y) = encode(x, fs, 5)    
+
+#reldif=np.abs(stft_x-stft_y)/(np.abs(stft_x)+1)
+#plot(reldif.sum(axis=0))
+
+#print('{}'.format(reldif.flatten().sum()))
+
+wmean=np.abs(np.log(w_x+.1)-np.log(w_y+.1)).flatten().mean()
+mel_x=librosa.feature.melspectrogram(x,fs)
+mel_y=librosa.feature.melspectrogram(y,fs)
+
+mel=np.abs(np.log(mel_x)-np.log(mel_y)).flatten().mean()
+
+print('wmean:%g, mel:%g'%(wmean,mel))
+#sa.play_buffer(x.astype('int16'),1,2,fs)
+
+#sa.play_buffer(y.astype('int16'),1,2,fs)
+
+#%%
+
+
+#mel_x=librosa.feature.melspectrogram(x,fs)
+#mel_y=librosa.feature.melspectrogram(y,fs)
+
+path="/home/eugening/Neural/MachineLearning/Speech/TrainingData/LJSpeech-1.0/wavs/LJ001-0003.wav"
+(fs,x1) = wav.read(path)
+x1=x1[0:x.size]
+vv=encode(x1,fs,5)
